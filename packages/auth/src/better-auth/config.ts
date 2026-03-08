c1e830fb6eda9b43557afb845de4694065af1a95
Charlemagne4 <mohdingo4@gmail.com>
2026-03-08 04:18:27 +0100

    refactor(auth): auth init and logic moved to packages

---
+++ packages/auth/src/better-auth/config.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+
+import { env } from '../../../../apps/web/src/env';
+import { db } from '@MyApp/db';
+import { nextCookies } from 'better-auth/next-js';
+
+export const auth = betterAuth({
+  baseURL: 'http://localhost:3000',
+  trustedOrigins: ['http://localhost:3000'],
+  database: prismaAdapter(db, {
+    provider: 'postgresql', // or "sqlite" or "mysql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
+      redirectUR: 'http://localhost:3000/api/auth/callback/github',
+    },
+  },
+  plugins: [nextCookies()], // add this
+});
+
+export type Session = typeof auth.$Infer.Session;
